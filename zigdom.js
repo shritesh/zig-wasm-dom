5acd9260ed6559aca81511fbaad07ad9f345a52b
Brian Ketelsen <mail@bjk.fyi>
2019-04-19 13:38:05 -0400

    pair session: add events proof of concept

--- zigdom.js
+++ zigdom.js
@@ -12,13 +12,69 @@ const getObject = function(objId) {
     return zigdom.objects[objId - 1];
 }
 
+const dispatch = function(eventId) {
+    return function() {
+        zigdom.exports.dispatchEvent(eventId);
+    };
+}
+
+const elementSetAttribute = function(node_id, name_ptr, name_len, value_ptr, value_len) {
+    const node = getObject(node_id);
+    const name = getString(name_ptr, name_len);
+    const value = getString(value_ptr, value_len);
+    node.setAttribute(name, value);
+}
+
+const elementGetAttribute = function(node_id, name_ptr, name_len, result_address_ptr, result_address_len_ptr) {
+    const node = getObject(node_id);
+    const attribute_name = getString(name_ptr, name_len);
+    const result = node[attribute_name];
+    // convert result into Uint8Array
+    const textEncoder = new TextEncoder();
+    const resultArray = textEncoder.encode(result);
+    var len = resultArray.length; 
+
+    if (len === 0) {
+        return false;
+    }
+
+
+    // allocate required number of bytes 
+    const ptr = zigdom.exports._wasm_alloc(len);
+    if (ptr === 0) {
+        throw "Cannot allocate memory";
+    }
+
+    // write the array to the memory
+    const mem_result = new DataView(zigdom.exports.memory.buffer, ptr, len);
+    for (let i = 0; i < len; ++i) {
+        mem_result.setUint8(i, resultArray[i], true);
+    }
+
+    // write the address of the result array to result_address_ptr
+    const mem_result_address = new DataView(zigdom.exports.memory.buffer, result_address_ptr, 32 / 8);
+    mem_result_address.setUint32(0, ptr, true);
+
+    //write the size of the result array to result_address_ptr_len_ptr
+    const mem_result_address_len = new DataView(zigdom.exports.memory.buffer, result_address_len_ptr, 32 / 8);
+    mem_result_address_len.setUint32(0, len, true);
+
+    // return if success? (optional)
+    return true;
+}
+const eventTargetAddEventListener = function(objId, event_ptr, event_len, eventId) {
+    const node = getObject(objId);
+    const ev = getString(event_ptr, event_len);
+    node.addEventListener(ev, dispatch(eventId));
+}
+
 const documentQuerySelector = function(selector_ptr, selector_len) {
-    selector = getString(selector_ptr, selector_len);
+    const selector = getString(selector_ptr, selector_len);
     return pushObject(document.querySelector(selector));
 }
 
 const documentCreateElement = function(tag_name_ptr, tag_name_len) {
-    tag_name = getString(tag_name_ptr, tag_name_len);
+    const tag_name = getString(tag_name_ptr, tag_name_len);
     return pushObject(document.createElement(tag_name));
 }
 
@@ -38,8 +94,13 @@ const nodeAppendChild = function(node_id, child_id) {
     return pushObject(node.appendChild(child));
 }
 
+const windowAlert = function(msg_ptr, msg_len) {
+    const msg = getString(msg_ptr, msg_len);
+    alert(msg);
+}
+
 const zigReleaseObject = function(object_id) {
-    zigdom.objects[object_id] = undefined;
+    zigdom.objects[object_id - 1] = undefined;
 }
 
 const launch = function(result) {
@@ -57,8 +118,18 @@ var zigdom = {
             create_element: documentCreateElement,
             create_text_node: documentCreateTextNode,
         },
+        element: {
+            set_attribute: elementSetAttribute,
+            get_attribute: elementGetAttribute
+        },
+        event_target: {
+            add_event_listener: eventTargetAddEventListener
+        },
         node: {
             append_child: nodeAppendChild
+        },
+        window: {
+            alert: windowAlert
         },
         zig: {
             release_object: zigReleaseObject
--- zigdom.zig
+++ zigdom.zig
@@ -1,9 +1,22 @@
 // https://github.com/shritesh/zig-wasm-dom
-extern "document" fn query_selector(selector_ptr: [*]u8, selector_len: usize) usize;
-extern "document" fn create_element(tag_name_ptr: [*]u8, tag_name_len: usize) usize;
-extern "document" fn create_text_node(data_ptr: [*]u8, data_len: usize) usize;
+extern "document" fn query_selector(selector_ptr: [*]const u8, selector_len: usize) usize;
+extern "document" fn create_element(tag_name_ptr: [*]const u8, tag_name_len: usize) usize;
+extern "document" fn create_text_node(data_ptr: [*]const u8, data_len: usize) usize;
+extern "element" fn set_attribute(element_id: usize, name_ptr: [*]const u8, name_len: usize, value_ptr: [*]const u8, value_len: usize,) void;
+extern "element" fn get_attribute(element_id: usize, name_ptr: [*]const u8, name_len: usize, value_ptr: *[*]u8, value_len: *usize) bool;
+extern "event_target" fn add_event_listener(event_target_id: usize, event_ptr: [*]const u8, event_len: usize, event_id: usize) void;
+extern "window" fn alert(msg_ptr: [*]const u8, msg_len: usize) void;
 extern "node" fn append_child(node_id: usize, child_id: usize) usize;
 extern "zig" fn release_object(object_id: usize) void;
+
+const std = @import("std");
+
+const eventId = enum(usize) {
+    Submit,
+    Clear
+};
+
+var input_tag_node: u32 = undefined;
 
 fn launch() !void {
     const body_selector = "body";
@@ -13,39 +26,130 @@ fn launch() !void {
     if (body_node == 0) {
         return error.QuerySelectorError;
     }
+    const input_tag_name = "input";
+    input_tag_node = create_element(&input_tag_name, input_tag_name.len);
+    // We don't release as we'll be referencing it later
 
-    const pre_tag_name = "pre";
-    const pre_tag_node = create_element(&pre_tag_name, pre_tag_name.len);
-    defer release_object(pre_tag_node);
-
-    if (pre_tag_node == 0) {
+    if (input_tag_node == 0) {
         return error.CreateElementError;
     }
-    
-    const text_msg = @embedFile("zigdom.zig");
-    const text_node = create_text_node(&text_msg, text_msg.len);
-    defer release_object(text_node);
 
-    if (text_node == 0) {
+    const input_tag_attribute_name = "value";
+    const input_tag_attribute_value= "Hello from Zig!";
+    set_attribute(input_tag_node, &input_tag_attribute_name, input_tag_attribute_name.len, &input_tag_attribute_value, input_tag_attribute_value.len);
+
+    const button_tag_name = "button";
+
+    const submit_button_node = create_element(&button_tag_name, button_tag_name.len);
+    defer release_object(submit_button_node);
+
+    if (submit_button_node == 0) {
+        return error.CreateElementError;
+    }
+
+    const event_name = "click";
+    attach_listener(submit_button_node, event_name, eventId.Submit);
+    
+    const submit_text_msg = "submit";
+    const submit_text_node = create_text_node(&submit_text_msg, submit_text_msg.len);
+    defer release_object(submit_text_node);
+
+    if (submit_text_node == 0) {
         return error.CreateTextNodeError;
     }
 
-    const attached_text_node = append_child(pre_tag_node, text_node);
-    defer release_object(attached_text_node);
+    const attached_submit_text_node = append_child(submit_button_node, submit_text_node);
+    defer release_object(attached_submit_text_node);
 
-    if (attached_text_node == 0) {
+    if (attached_submit_text_node == 0) {
         return error.AppendChildError;
     }
 
-    const attached_pre_node = append_child(body_node, pre_tag_node);
-    defer release_object(attached_pre_node);
+    const clear_button_node = create_element(&button_tag_name, button_tag_name.len);
+    defer release_object(clear_button_node);
 
-    if (attached_pre_node == 0) {
+    if (clear_button_node == 0) {
+        return error.CreateElementError;
+    }
+
+    attach_listener(clear_button_node, event_name, eventId.Clear);
+
+    const clear_text_msg = "clear";
+    const clear_text_node = create_text_node(&clear_text_msg, clear_text_msg.len);
+    defer release_object(clear_text_node);
+
+    if (clear_text_node == 0) {
+        return error.CreateTextNodeError;
+    }
+
+    const attached_clear_text_node = append_child(clear_button_node, clear_text_node);
+    defer release_object(attached_clear_text_node);
+
+    if (attached_clear_text_node == 0) {
         return error.AppendChildError;
+    }
+
+    const attached_input_node = append_child(body_node, input_tag_node);
+    defer release_object(attached_input_node);
+
+    if (attached_input_node == 0) {
+        return error.AppendChildError;
+    }
+
+    const attached_submit_button_node = append_child(body_node, submit_button_node);
+    defer release_object(attached_submit_button_node);
+
+    if (attached_submit_button_node == 0) {
+        return error.AppendChildError;
+    }
+
+    const attached_clear_button_node = append_child(body_node, clear_button_node);
+    defer release_object(attached_clear_button_node);
+
+    if (attached_clear_button_node == 0) {
+        return error.AppendChildError;
+    }
+    
+}
+
+fn attach_listener(node: usize, event_name: []const u8, event_id: eventId) void {
+    add_event_listener(node, event_name.ptr, event_name.len, @enumToInt(event_id));
+
+}
+
+export fn dispatchEvent(id: u32) void {
+    switch(@intToEnum(eventId, id)) {
+        eventId.Submit => on_submit_event(),
+        eventId.Clear => on_clear_event(),
+    }
+}
+
+fn on_clear_event() void {
+    const input_tag_attribute_name = "value";
+    const input_tag_attribute_value= "";
+    set_attribute(input_tag_node, &input_tag_attribute_name, input_tag_attribute_name.len, &input_tag_attribute_value, input_tag_attribute_value.len);
+}
+fn on_submit_event() void {
+    var attribute_ptr: [*]u8 = undefined;
+    var attribute_len: usize = undefined;
+
+    const input_tag_attribute_name = "value";
+    const success = get_attribute(input_tag_node, &input_tag_attribute_name, input_tag_attribute_name.len, &attribute_ptr, &attribute_len);
+
+    if (success) {
+        const result = attribute_ptr[0..attribute_len];
+        defer std.heap.wasm_allocator.free(result);
+
+        alert(result.ptr, result.len);
     }
 }
 
 export fn launch_export() bool {
     launch() catch |err| return false;
     return true;
+}
+
+export fn _wasm_alloc(len: usize) u32 {
+    var buf = std.heap.wasm_allocator.alloc(u8, len) catch |err| return 0;
+    return @ptrToInt(buf.ptr);
 }
